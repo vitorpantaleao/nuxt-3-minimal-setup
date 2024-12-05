0aed7a387b330c6a983a35af2fd471ae2cd13060
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-12-04 23:11:13 -0300

    feat: add shadcn components for tests

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
     <h1>Olá</h1>
+    <Button>Click me</Button>
+    <Calendar v-model="value" :weekday-format="'short'" class="rounded-md border" />
   </div>
 </template>
+
+<script setup>
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { getLocalTimeZone, today } from '@internationalized/date';
+
+const value = ref(today(getLocalTimeZone()));
+</script>

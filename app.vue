dbfa4b9b0ce1f21f62f9015a2ba14963516d8655
Vitor Pantaleão <vitor-hmp@hotmail.com>
2024-12-04 23:32:02 -0300

    chore: Refactor app.vue to use max-w-max class for Calendar component

--- app.vue
+++ app.vue
@@ -2,18 +2,14 @@
     <div>
         <h1>Olá</h1>
         <Button>Click me</Button>
-        <Calendar
-            v-model="value"
-            :weekday-format="'short'"
-            class="rounded-md border"
-        />
+        <Calendar v-model="value" :weekday-format="'short'" class="rounded-md border max-w-max" />
     </div>
 </template>
 
 <script setup>
-    import { Button } from '@/components/ui/button';
-    import { Calendar } from '@/components/ui/calendar';
-    import { getLocalTimeZone, today } from '@internationalized/date';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { getLocalTimeZone, today } from '@internationalized/date';
 
-    const value = ref(today(getLocalTimeZone()));
+const value = ref(today(getLocalTimeZone()));
 </script>
